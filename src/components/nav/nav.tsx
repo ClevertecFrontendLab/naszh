ea677334be8db0d58eecd971b664814bce404db8
naszh <naszh99@gmail.com>
2025-04-10 11:05:34 +0300

    fix(nav): add custom scroll and footer

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,22 +1,37 @@
-import { Accordion } from '@chakra-ui/react';
+import '../../styles/custom-scroll.css';
 
+import { Accordion, Box, Button, Flex, Stack, Text } from '@chakra-ui/react';
+
+import { ExitIcon } from '../nav-icons/exit';
 import { NavItem } from './nav-item';
 import { navItems } from './nav-items';
 
 export const Nav = () => (
-    // <Stack pt={6} justifyContent={'space-between'} h={'100%'} className={styles.container}>
-    <Accordion allowToggle w='256px' pt={2.5} pl={2.5} pr={4} mt={6}>
-        {navItems.map((item, i) => (
-            <NavItem {...item} key={i} />
-        ))}
-    </Accordion>
-    // <Stack p={6}>
-    //     <Text color='blackAlpha.400' fontSize='xs' lineHeight={4} fontWeight={500}>
-    //         Версия программы 03.25
-    //     </Text>
-    //     <Text color='blackAlpha.700' fontSize='xs' lineHeight={4}>
-    //         Все права защищены, ученический файл, <br /> ©Клевер Технолоджи, 2025
-    //     </Text>
-    // </Stack>
-    // </Stack>
+    <Flex direction='column' justifyContent='space-between' h='calc(100vh - 80px)' w='256px'>
+        <Box className='custom-scroll'>
+            <Accordion allowToggle pt={2.5} pl={2.5} pr={4} mt={6}>
+                {navItems.map((item, i) => (
+                    <NavItem {...item} key={i} />
+                ))}
+            </Accordion>
+        </Box>
+        <Stack p={6} gap={4}>
+            <Text color='blackAlpha.400' fontSize='xs' lineHeight={4} fontWeight={500}>
+                Версия программы 03.25
+            </Text>
+            <Text color='blackAlpha.700' fontSize='xs' lineHeight={4}>
+                Все права защищены, ученический файл, <br /> ©Клевер Технолоджи, 2025
+            </Text>
+            <Button
+                leftIcon={<ExitIcon />}
+                variant='link'
+                alignSelf='flex-start'
+                fontSize='12px'
+                fontWeight={600}
+                color='000'
+            >
+                Выйти
+            </Button>
+        </Stack>
+    </Flex>
 );
