239d18ff7dc905b2b554cf4cc5a9fb4f0d6b6337
naszh <naszh99@gmail.com>
2025-04-11 17:33:38 +0300

    fix(search): update search component view

---
+++ src/components/icons/switch-icon.tsx
@@ -0,0 +1,12 @@
+import { createIcon } from '@chakra-ui/icons';
+
+export const SwitchIcon = createIcon({
+    displayName: 'SwitchIcon',
+    viewBox: '0 0 34 20',
+    path: (
+        <>
+            <rect width='34' height='20' rx='10' fill='black' fill-opacity='0.16' />
+            <circle cx='10' cy='10' r='8' fill='white' />
+        </>
+    ),
+});
--- src/components/search-list/search-list.tsx
+++ src/components/search-list/search-list.tsx
@@ -1,18 +1,33 @@
-import { Button, Flex, Heading, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import {
+    Box,
+    Button,
+    Flex,
+    Heading,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Select,
+    Text,
+} from '@chakra-ui/react';
 
 import { FilterBtn } from '../icons/filter-btn';
+import { SearchIcon } from '../icons/search-icon';
+import { SwitchIcon } from '../icons/switch-icon';
 
 export const SearchList = () => (
     <Flex
         width='1360px'
         flexDirection='column'
         alignItems='center'
-        gap='32px'
+        gap='16px'
         m='32px 0'
         h='fit-content'
     >
-        <Heading fontSize='48px'>Приятного аппетита!</Heading>
-        <Flex>
+        <Heading fontSize='48px' mb={4}>
+            Приятного аппетита!
+        </Heading>
+        <Flex w='518px' gap='12px'>
             <Button
                 border='1px solid'
                 borderColor='blackAlpha.600'
@@ -20,13 +35,34 @@ export const SearchList = () => (
                 p='0 12px'
                 w='48px'
                 h='48px'
+                flexShrink={0}
             >
                 <FilterBtn boxSize='24px' />
             </Button>
             <InputGroup>
-                <InputRightElement pointerEvents='none'></InputRightElement>
-                <Input placeholder='Название или ингредиент...' />
+                <InputRightElement
+                    pointerEvents='none'
+                    h='48px'
+                    children={<SearchIcon boxSize='18px' />}
+                />
+                <Input
+                    placeholder='Название или ингредиент...'
+                    h='48px'
+                    borderColor='blackAlpha.600'
+                />
             </InputGroup>
+        </Flex>
+        <Flex w='518px' gap='16px'>
+            <Box display='flex' gap='12px' alignItems='center' flexShrink={0} p='6px 0px 6px 8px'>
+                <Text fontWeight={500}>Исключить мои аллергены</Text>
+                <SwitchIcon boxSize='34px' />
+            </Box>
+            <Select
+                icon={<ChevronDownIcon />}
+                variant='outline'
+                placeholder='Выберите из списка'
+                alignSelf='center'
+            />
         </Flex>
     </Flex>
 );
